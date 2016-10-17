b0e5013a063ee3c860cec62eff41e564982d444e
Microsanz Macromath <microsanz.macromath@yahoo.com>
2016-10-18 01:25:23 +0700

    colored combobox

--- Senin_141110086_Sanjaya/Latihan_3_1/Form1.Designer.cs
+++ Senin_141110086_Sanjaya/Latihan_3_1/Form1.Designer.cs
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.comboBox1_DrawItem);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(330, 297);
+            this.ClientSize = new System.Drawing.Size(144, 46);
+            this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- Senin_141110086_Sanjaya/Latihan_3_1/Form1.cs
+++ Senin_141110086_Sanjaya/Latihan_3_1/Form1.cs
@@ -12,9 +12,37 @@ namespace Latihan_3_1
 {
     public partial class Form1 : Form
     {
+        private List<coloredComboObject> lstColor;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lstColor = coloredComboItems.GetColors();
+            comboBox1.DataSource = lstColor ;
+            comboBox1.ValueMember = "Color";
+            comboBox1.DisplayMember = "Name";
+        }
+        private void comboBox1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            e.DrawBackground();
+            if (e.Index >= 0)
+            {
+                Color clr = lstColor[e.Index].Color;
+                using (Brush brush = new SolidBrush(clr))
+                {
+                    e.Graphics.FillRectangle(brush, e.Bounds.X, e.Bounds.Y, 14, 14);
+                }
+
+                using (Brush brush = new SolidBrush(e.ForeColor))
+                {
+                    e.Graphics.DrawString(lstColor[e.Index].Name.ToString(), e.Font, brush, e.Bounds.Left + 16, e.Bounds.Top);
+                }
+                e.DrawFocusRectangle();
+
+            }
         }
     }
 }
